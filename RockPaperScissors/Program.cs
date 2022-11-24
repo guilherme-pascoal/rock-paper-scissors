0176992d581342f02e6eee837983c3b9dd0e92d7
Guilherme Pascoal <gui_pascoalcruz@hotmail.com>
2022-11-24 15:57:38 -0300

    Jogo Pedra Papel Tesoura

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Players 1: ");
+            string p1 = Console.ReadLine();
+            Console.Write("Players 2: ");
+            string p2 = Console.ReadLine();
+
+            string result = ResultRockPaperScissors(p1, p2);
+            Console.WriteLine(result);
+        }
+
+        static string ResultRockPaperScissors(string p1, string p2)
+        {
+            if (p1 == p2) return "Draw!";
+
+            int p = (p1 + p2) == "rockscissors" || (p1 + p2) == "scissorspaper" || (p1 + p2) == "paperrock" ? 1 : 2;
+            return "Player " + p + " won!";
+        }
+    }
+}
